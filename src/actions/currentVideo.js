58e5305c39b2060410b73041e6239df9b1878228
Kristie Desiree <kly5@mail.sfsu.edu>
2018-08-28 16:15:00 -0700

    Basic structure of actions

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,10 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  return {
+    type: 'SET_CURRENT_VIDEO',
+    video: video, //object
+  }
 });
 
 export default changeVideo;
+w
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,9 @@
 var changeVideoList = (videos) => ({
   //TODO:  Return some action object to change the list of videos in VideoList.
+  return {
+    type: 'CHANGE_VIDEO_LIST',
+    videoList: videos, //array
+  }
 });
 
 export default changeVideoList;
