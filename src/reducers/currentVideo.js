d406a283e2a3637a72d57fc4908dfa6db4284c8f
Kristie Desiree <kly5@mail.sfsu.edu>
2018-08-28 16:25:39 -0700

    Apply logic for reducer functions

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -2,6 +2,16 @@ import Redux from 'redux';
 
 var currentVideoReducer = (state, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  if ('SET_CURRENT_VIDEO') {
+    return action.video;
+  } else {
+    return state;
+  }
 };
 
+//pass in previous state and action
+//if action type matches (video was changed)
+  //set new state (set current video to the new video)
+//else
+  //return original state
 export default currentVideoReducer;
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -2,6 +2,17 @@ import Redux from 'redux';
 
 var videoListReducer = (state = sampleData, action) => {
   //TODO: define a reducer for the videoList field of our state.
+  if ('CHANGE_VIDEO_LIST') {
+    return action.videoList;
+  } else {
+    return state;
+  }
 };
 
 export default videoListReducer;
+
+//pass in previous state and action
+//if action type matches (video list was changed)
+  //set new state (set current video list to the new video list)
+//else
+  //return original state
